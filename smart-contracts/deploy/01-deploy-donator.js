1d6f7fa47ced5c38b8a96120e8224869d66e363b
Aditya Bhattad <adityabhattad18@gmail.com>
2023-02-13 21:40:24 +0530

    Add deploy script

--- smart-contracts/deploy/01-deploy-donator.js
+++ smart-contracts/deploy/01-deploy-donator.js
@@ -1,5 +1,41 @@
-module.exports=async ()=>{
-    console.log("hello");
+const { network } = require("hardhat");
+const { developmentChains } = require("../helper-hardhat-config");
+require("dotenv").config();
+const fs = require("fs");
+
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+    console.log(`------Deployer is ${deployer}------------`);
+
+
+    const chainId = network.config.chainId;
+    console.log(`------ChainId is ${chainId}------------`);
+    log("----------------------------------------------------")
+    log(`1) Deploying DonatorAccessToken...`);
+    const donatorAccessToken = await deploy("DonatorAccess", {
+        from: deployer,
+        args: [],
+        log: true,
+        waitConfirmations:network.config.blokConfirmations || 1,
+    });
+    log("Successfully deployed DonatorAccessToken");
+    log("----------------------------------------------------")
+    
+    
+    log("----------------------------------------------------")
+    log(`2) Deploying Donator...`);
+    await deploy("Donator", {
+        from: deployer,
+        args: [donatorAccessToken.address],
+        log: true,
+        waitConfirmations:network.config.blokConfirmations || 1,
+    });
+    log("Successfully deployed Donator");
+    log("----------------------------------------------------")
+
 }
 
-module.exports.tags=["main"]
+
+module.exports.tags = ["all"];
--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,9 +1,50 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("hardhat-deploy")
-require("dotenv").config()
+require("hardhat-deploy");
+require("dotenv").config();
 
+
+const POLYGON_RPC_URL = process.env.POLYGON_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const POLYSCAN_API_KEY = process.env.POLYSCAN_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+	solidity: "0.8.17",
+	defaultNetwork: "hardhat",
+	networks: {
+		hardhat: {
+			chainId: 1337,
+			blockConfirmations:1,
+		},
+		localhost: {
+			chainId: 1337,
+			blockConfirmations: 1,
+		},
+		mumbai: {
+			chainId: 80001,
+			blockConfirmations: 6,
+			url: POLYGON_RPC_URL,
+			accounts: [PRIVATE_KEY],
+		},
+	},
+	namedAccounts: {
+		deployer: {
+			default: 0,
+		},
+		contestant1: {
+			default: 1,
+		},
+		contestant2: {
+			default: 2,
+		},
+	},
+	settings: {
+		optimizer: {
+			enabled: true,
+			runs: 200,
+		},
+	},
+	mocha: {
+		timeout: 500000, // 500 seconds.
+	},
 };
